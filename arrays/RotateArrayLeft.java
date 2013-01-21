f74a3947ff4d441ba0b712379a788190e5474268
John Kurlak <john@kurlak.com>
2013-01-21 00:57:32 -0500

    Fixed bug in array rotate code

--- arrays/RotateArrayLeft.java
+++ arrays/RotateArrayLeft.java
@@ -1,6 +1,6 @@
 /**
- * This program rotates all of the elements in an array left by a given
- * k value.  It runs in O(n) time and uses O(1) additional space (it operates
+ * This program rotates all of the elements in an array left by a given k
+ * value.  It runs in O(n) time and uses O(1) additional space (it operates
  * in-place).
  *
  * @author      John Kurlak <john@kurlak.com>
@@ -14,7 +14,7 @@ public class RotateArrayLeft {
      */
     public static void main(String[] args) {
         int[] array = new int[] { 5, 3, 2, 18, 20 };
-        int k = 3;
+        int k = 2;
 
         printArray(array);
         System.out.println("rotated to the left " + k + " is:");
@@ -37,18 +37,46 @@ public class RotateArrayLeft {
         }
 
         int n = array.length;
-        int origIndex = 0;
 
-        // Place each array item into its rotated index
-        for (int i = 0; i < n; i++) {
-            int shiftedIndex = (origIndex - k) % n;
-            if (shiftedIndex < 0) {
-                shiftedIndex += n;
-            }
-
-            swap(array, 0, shiftedIndex);
-            origIndex = shiftedIndex;
+        // Handle negative k values (rotate to right)
+        if (k < 0) {
+            k = n - Math.abs(k);
         }
+
+        // Ensure k is in interval [0, n)
+        k %= n;
+
+        // Rotate the array
+        reverseArray(array, 0, k - 1);
+        reverseArray(array, k, n - 1);
+        reverseArray(array, 0, n - 1);
+    }
+
+    /**
+     * Reverses all values in an array from the given start index to the given end index.
+     * The reversal happens in-place, so there is no return value.
+     *
+     * @param array         The array to reverse.
+     * @param startIndex    The start index of the subarray to reverse.
+     * @param endIndex      The end index of the subarray to reverse.
+     */
+    public static void reverseArray(int[] array, int startIndex, int endIndex) {
+        for (int leftIndex = startIndex, rightIndex = endIndex; leftIndex < rightIndex; leftIndex++, rightIndex--) {
+            swap(array, leftIndex, rightIndex);
+        }
+    }
+
+    /**
+     * Swaps two elements in a list.
+     *
+     * @param list      The list.
+     * @param index1    The index of the first element to swap.
+     * @param index2    The index of the second element to swap.
+     */
+    public static void swap(int[] list, int index1, int index2) {
+        int temp = list[index1];
+        list[index1] = list[index2];
+        list[index2] = temp;
     }
 
     /**
@@ -70,18 +98,5 @@ public class RotateArrayLeft {
         }
 
         System.out.println("}");
-    }
-
-    /**
-     * Swaps two elements in a list.
-     *
-     * @param list      The list.
-     * @param index1    The index of the first element to swap.
-     * @param index2    The index of the second element to swap.
-     */
-    public static void swap(int[] list, int index1, int index2) {
-        int temp = list[index1];
-        list[index1] = list[index2];
-        list[index2] = temp;
     }
 }
